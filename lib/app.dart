c88be0cd5037f078026f5cf03a7b54759bf881b0
anvarjonovbobirbek <anvarjonovbob1999@mail.ru>
2022-07-31 10:51:38 +0500

    fix: add deleted package splash
    fix: move create_account_date_page to create account package

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/features/login/presentation/pages/creat_account_date_page.dart';
+import 'package:flutter_template/features/login/presentation/pages/create_account/creat_account_date_page.dart';
 
 
 class MyApp extends StatelessWidget {
---
+++ lib/features/login/presentation/pages/splash/slpash_page.dart
@@ -0,0 +1,38 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_template/app/app_colors.dart';
+import 'package:flutter_template/app/app_icons.dart';
+
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:AppColors.BUTTON_BLUE,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            AppIcons.LOGO,
+            width: 150,
+            color: Colors.white,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 65.0, left: 65.0),
+            child: Text(
+              "DWED",
+              style: TextStyle(
+                fontSize: 95,
+                color: AppColors.WHITE,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
